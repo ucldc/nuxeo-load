1664d7f8b1a20ffe1e9d7fe8c2780573bcc51bc1
Brian Tingle <brian.tingle@ucop.edu>
2013-12-06 08:33:29 -0800

    batch load

---
+++ UCIHistoryFilmsAndVideos/batch_load.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
+. $DIR/../nuxeo-fileImporter-api.sh
+
+collection=UCIHistoryFilmsAndVideos
+leafType=CustomVideo
+campus=UCI
+
+api_get "logActivate"
+api_get "run?leafType=$leafType&inputPath=/apps/content/new_path/$campus/$collection&targetPath=asset-library/$campus&folderishType=Organization"
+api_get "status"
+api_get "log"
