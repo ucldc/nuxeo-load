1f9e8ae44f8792a916947a9f7ebc16ea7902908a
Brian Tingle <brian.tingle@ucop.edu>
2013-12-06 08:01:29 -0800

    these things don't belong in a shell

--- nuxeo-fileImporter-api.sh
+++ nuxeo-fileImporter-api.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-set -o pipefail  # trace ERR through pipes
-set -o errtrace  # trace ERR through 'time command' and other functions
-set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
-set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
-
 # change the default password and don't commit to revision control
 NUXEO_ADMIN_PASS=${NUXEO_ADMIN_PASS="Administrator"}
 
