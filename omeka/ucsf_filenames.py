0e8ccf5f525e1693234f2473efb96514db4fcffc
Barbara Hui <barbara.hui@ucop.edu>
2014-06-03 16:33:43 -0700

    Updated script.

--- omeka/ucsf_filenames.py
+++ omeka/ucsf_filenames.py
@@ -3,12 +3,13 @@ import omnux
 from pynux import utils
 import pprint
 import json
+import os
 
 api_url = 'https://digital.library.ucsf.edu/api/'
+outputdir = "/apps/nuxeo/code/nuxeo-load/relink/ucsf-omeka/paths"
 
 def main():
-  #collection_ids = [13,10,6,9,14,8]
-  collection_ids = [13]
+  collection_ids = [13,10,6,9,14,8]
   nx = utils.Nuxeo()
 
   for collection_id in collection_ids:
@@ -16,12 +17,17 @@ def main():
       collection_mapping_data = json.load(cf)
     
     collection_name = omnux.get_collection_property(collection_mapping_data, collection_id, "name") 
+    output_file = os.path.join(outputdir, collection_name)
+    try:
+        os.remove(output_file)
+    except OSError:
+        pass
 
     items_metadata = omnux.extract_items(api_url, collection_id)
-    for item in items_metadata:
-      path = omnux.get_item_filename(item) 
-      print path
-
+    with open(output_file, "a+") as f:
+        for item in items_metadata:
+            path = omnux.get_item_filename(item) 
+            f.write(path + "\n")
 
 if __name__ == '__main__':
   main()
