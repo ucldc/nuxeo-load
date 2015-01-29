65c0975fd5bb70dd327f7b58723b4ee3daa36e18
Brian Tingle <brian.tingle@ucop.edu>
2015-01-28 22:04:56 -0800

    add --loglevel option

--- s3_data_migration/s3_up.py
+++ s3_data_migration/s3_up.py
@@ -7,9 +7,12 @@ import os
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from pprint import pprint as pp
+import logging
+import time
 
 
 def main(argv=None):
+    _loglevel_='WARNING'
     parser = argparse.ArgumentParser(description='migrate nuxeo data to s3')
     parser.add_argument('--binaries_data',
                         required=True,
@@ -17,10 +20,24 @@ def main(argv=None):
     parser.add_argument('--bucket',
                         required=True,
                         help="s3 bucket on AWS")
+    parser.add_argument(
+        '--loglevel',
+        default= _loglevel_,
+        help=''.join(["CRITICAL ERROR WARNING INFO DEBUG NOTSET, default is ",_loglevel_])
+    )
+
     if argv is None:
         argv = parser.parse_args()
 
-    # check that --binaries_data is a nuxeo dir?
+    # set debugging level
+    numeric_level = getattr(logging, argv.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % argv.loglevel)
+    logging.basicConfig(level=numeric_level, )
+    # log some stuff
+    logger = logging.getLogger(__name__)
+
+    # check that --binaries_data is a nuxeo dir? TODO?
 
     # find all the files in the directory
     for (dirpath, ____, filenames) in os.walk(argv.binaries_data):
@@ -28,7 +45,8 @@ def main(argv=None):
             # upload to s3
             check_file(os.path.join(dirpath, filename),
                        filename,
-                       argv.bucket)
+                       argv.bucket,
+                       logger)
 
 
 class FileSizeMismatch(Exception):
@@ -39,7 +57,7 @@ class FixityCheckError(Exception):
     pass
 
 
-def check_file(path, filename, bucket_name):
+def check_file(path, filename, bucket_name, logger):
     """check if file has been uploaded; if so, double checks size"""
     conn = S3Connection()
     bucket = conn.get_bucket(bucket_name)
@@ -50,27 +68,32 @@ def check_file(path, filename, bucket_name):
         # looks like it is here; let's double check the size
         if os.stat(path).st_size != key.size:
             raise FileSizeMismatch('key exists; but sizes mis-match')
+        logging.info('{0} skipped'.format(filename))
     else:
+        start = time.time()
         upload(path, filename, size, bucket)
+        seconds = time.time() - start
+        logging.info(
+            '{0} uploaded {1:.2E} bytes in {2:.2E} seconds {3:.2E} bytes/second'.format(
+                filename,
+                size,
+                seconds,
+                size//seconds
+            )
+        )
 
 
 def upload(path, filename, size, bucket):
     """upload file and double check size"""
     k = Key(bucket)
     k.key = filename
-    k.set_contents_from_filename(path, cb=percent_cb, num_cb=10)
+    k.set_contents_from_filename(path)
     # double check the size
     if k.size != size:
         raise FileSizeMismatch('something is very wrong')
     # check the md5
     if k.md5 != filename:
         raise FixityCheckError('checksums mismatch {0}!={1}'.format(k.md5, filename))
-
-
-def percent_cb(complete, total):
-    #sys.stdout.write(complete)
-    sys.stdout.write("\r%d%%" % complete)
-    sys.stdout.flush()
 
 
 # main() idiom for importing into REPL for debugging
