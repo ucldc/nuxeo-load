601b5148536117ee44076ce34dcc61fe47ce10f3
Barbara Hui <barbara.hui@ucop.edu>
2021-10-20 17:48:13 -0700

    Update for a few more collections

--- ucispace/create_payload_handle_id.py
+++ ucispace/create_payload_handle_id.py
@@ -6,8 +6,18 @@ import json
 SETS = {
     'hdl_10575_12033': {
         'nuxeo_folder': '/asset-library/UCI/SCA_UniversityArchives/Publications/Anthill'
-        }
+        },
+    'hdl_10575_11968': {
+        'nuxeo_folder': '/asset-library/UCI/SCA_UniversityArchives/Publications/Yearbooks'
+        },
+    'hdl_10575_8408': {
+        'nuxeo_folder': '/asset-library/UCI/SCA_UniversityArchives/Publications/Promotional Publications'
+        },
+    'hdl_10575_5263': {
+        'nuxeo_folder': '/asset-library/UCI/SCA_UniversityArchives/Publications/New University'
+        },
     }
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -35,15 +45,47 @@ def main():
         handle_id = [identifier for identifier in record.get('identifier') if 'hdl.handle.net' in identifier][0]
 
         # get something to match on
-        pdf_filename = [os. path. basename(cr['ref']) for cr in record.get('component_resources')][0]
-        if not pdf_filename.startswith('LD781I7S65_'):
-            pdf_filename = f"LD781I7S65_{pdf_filename}"
+        pdf_filename = [os.path.basename(cr['ref']) for cr in record.get('component_resources')]
+        if pdf_filename:
+            pdf_filename = pdf_filename[0]
+        else:
+            print(f"No pdf_filename for {record['identifier']=}")
+
+        if setid == 'hdl_10575_12033':
+            if not pdf_filename.startswith('LD781I7S65_'):
+                pdf_filename = f"LD781I7S65_{pdf_filename}"
+        elif setid == 'hdl_10575_11968':
+            pdf_filename.lstrip('LD781I7S65_')
+            pdf_filename = f"{pdf_filename[0:18]}.pdf"
+        elif setid == 'hdl_10575_8408':
+            if pdf_filename == 'UCITheFirstDecade.pdf':
+                pdf_filename = 'The First Decade%3A 1965-1975.1491586207490'
+        elif setid == 'hdl_10575_5263':
+            if 'http://hdl.handle.net/10575/6084' in handle_id:
+                pdf_filename = 'LH1C215S64_19710219.pdf'
+            elif 'http://hdl.handle.net/10575/6360' in handle_id:
+                pdf_filename = 'LH1C215S64_19760406.pdf'
+            elif 'http://hdl.handle.net/10575/11995' in handle_id:
+                pdf_filename = 'uci_newspapers_satire_vol-16_screw_university.pdf'
+            elif 'http://hdl.handle.net/10575/11998' in handle_id:
+                pdf_filename = 'uci_newspapers_satire_vol-17_phew_university.pdf'
+            elif 'http://hdl.handle.net/10575/11996' in handle_id:
+                pdf_filename = 'uci_newspapers_satire_vol-22_the_only_alternative.pdf'
+            elif 'http://hdl.handle.net/10575/11997' in handle_id:
+                pdf_filename = 'uci_newspapers_satire_vol-23_the_pee_u.pdf'
+
+            if pdf_filename.endswith('_pdfa.pdf'):
+                pdf_filename = pdf_filename.rstrip('_pdfa.pdf')
+                pdf_filename = f"LH1C215S64_{pdf_filename}.pdf"
+
+        title = record.get('title')[0]
 
         items.append(
                 {
                     'handle_id': handle_id, 
                     'pdf_filename': pdf_filename,
-                    'nuxeo_folder': SETS[setid]['nuxeo_folder']
+                    'nuxeo_folder': SETS[setid]['nuxeo_folder'],
+                    'title': title
                 }
             )
 
--- ucispace/update_handle_ids.py
+++ ucispace/update_handle_ids.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import argparse
 from nuxeo.client import Nuxeo
+from nuxeo.exceptions import HTTPError
 import json
 
 API_BASE = 'https://nuxeo.cdlib.org/Nuxeo/site'
@@ -12,7 +13,7 @@ NUXEO_PASSWORD = os.environ.get('NUXEO_PASS')
 
 def main():
     logger = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser(
         description='Update Nuxeo objects with handle IDs')
@@ -40,13 +41,25 @@ def main():
         pdf_filename = item['pdf_filename']
         doc_path = f"{nuxeo_folder}/{pdf_filename}"
         doc_url = f"{API_BASE}/{API_PATH}/path{nuxeo_folder}/{pdf_filename}"
+        handle_id = item['handle_id']
+        try:
+            doc = nuxeo.documents.get(path=doc_path)
+        except HTTPError:
+            if setid == 'hdl_10575_5263':
+                try:
+                    alt_filename = f"LH1C215S64_{pdf_filename}"
+                    doc_path = f"{nuxeo_folder}/{alt_filename}"
+                    doc = nuxeo.documents.get(path=doc_path)
+                except HTTPError:
+                    print(f"Nuxeo doc does not exist for {pdf_filename} OR {alt_filename}, {item['title']}, {handle_id}")
+                    continue
 
-        doc = nuxeo.documents.get(path=doc_path)
         local_id = doc.get('ucldc_schema:localidentifier')
-        local_id.append(item['handle_id'])
-        doc.properties['ucldc_schema:localidentifier'] = local_id
-        doc.save()
-
+        if handle_id not in local_id:
+            local_id.append(item['handle_id'])
+            doc.properties['ucldc_schema:localidentifier'] = local_id
+            doc.save()
+            print(f"updated {doc_path}")
 
 if __name__ == '__main__':
     sys.exit(main())
